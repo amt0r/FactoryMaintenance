97a8c84ed8b4f91cf93182765212ccf8746cbccd
amt0r <mr.felchin@gmail.com>
2026-07-30 21:47:51 +0300

    Separate files and plugins in build-logic and add JvmLibraryConventionPlugin

---
+++ build-logic/convention/src/main/kotlin/com/factorymaintenance/KotlinAndroid.kt
@@ -0,0 +1,51 @@
+package com.factorymaintenance
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.assign
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinBaseExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension,
+) {
+    commonExtension.apply {
+        compileSdk = 37
+
+        defaultConfig.apply {
+            minSdk = 29
+        }
+
+        compileOptions.apply {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+    }
+
+    configureKotlin<KotlinAndroidProjectExtension>()
+}
+
+
+internal fun Project.configureKotlinJvm() {
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    configureKotlin<KotlinJvmProjectExtension>()
+}
+
+private inline fun <reified T : KotlinBaseExtension> Project.configureKotlin() = configure<T> {
+    when (this) {
+        is KotlinAndroidProjectExtension -> compilerOptions
+        is KotlinJvmProjectExtension -> compilerOptions
+        else -> TODO("Unsupported project extension $this ${T::class}")
+    }.apply {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
